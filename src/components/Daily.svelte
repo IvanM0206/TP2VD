166c1ad6f88d60c3107d8d8092c08751c2d36a5d
Fede Peitti <fede.peitti@gmail.com>
2024-07-08 22:38:21 -0300

    Iframe import of maps.

---
+++ src/components/Daily.svelte
@@ -0,0 +1,39 @@
+<main>
+  <div
+    style="padding-left: 35px;"
+    class="flourish-embed flourish-chart"
+    data-src="visualisation/18481701"
+  >
+    <script src="https://public.flourish.studio/resources/embed.js"></script>
+  </div>
+
+  <p style="padding-left: 35px;">
+    También su expansión por el mundo permite descubrir dónde impactó más.
+    Debajo podés ver por país el porcentaje de usuarios que usan Chat GPT
+    diariamente. Entre los países que más frecuentemente utilizan el chat están,
+    por ejemplo, <span>India y Kenia</span>.
+  </p>
+  <iframe
+    src="https://flo.uri.sh/visualisation/18439115/embed"
+    title="Interactive or visual content"
+    class="flourish-embed-iframe"
+    frameborder="0"
+    scrolling="no"
+    style="width:100%;height:600px;"
+    sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
+  ></iframe>
+  <p style="padding-left: 35px;">
+    Alrededor de todo el planeta se ha ido extendiendo la tendencia de utilizar
+    estas nuevas herramientas para diferentes aplicaciones. Un uso consciente de
+    las mismas puede colaborar y acelerar grandes proyectos, pero sin el debido
+    conocimiento se corre el riesgo de ralentizar operaciones e incluso
+    restringirse a un círculo limitado de conocimiento personal.
+  </p>
+</main>
+
+<style>
+  span {
+    color: #15fcd8;
+    font-weight: bold;
+  }
+</style>
--- src/components/MapaAwareness.svelte
+++ src/components/MapaAwareness.svelte
@@ -1,5 +1,11 @@
 <main>
-  <div class="flourish-embed flourish-webgl" data-src="visualisation/18516918">
-    <script src="https://public.flourish.studio/resources/embed.js"></script>
-  </div>
+  <iframe
+    src="https://flo.uri.sh/visualisation/18516918/embed"
+    title="Interactive or visual content"
+    class="flourish-embed-iframe"
+    frameborder="0"
+    scrolling="no"
+    style="width:100%;height:600px;"
+    sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
+  ></iframe>
 </main>
