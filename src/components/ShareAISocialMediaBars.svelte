ce80e224a3cb4b31fc987135de6b359dfed4a5dd
Ivan Mondrzak <45753638@utdt.local>
2024-07-09 14:27:10 -0300

    grafico arreglado

--- src/components/ShareAISocialMediaBars.svelte
+++ src/components/ShareAISocialMediaBars.svelte
@@ -1,5 +1,5 @@
 <main>
-  <div class="flourish-embed flourish-chart" data-src="visualisation/18663993"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
+  <iframe src='https://flo.uri.sh/visualisation/18663993/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/18663993/?utm_source=embed&utm_campaign=visualisation/18663993' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
   <p style="padding-left:35px;">
     El gráfico muestra que <span>GPT-4</span> lidera con un impresionante
     <span>45% de índice de popularidad</span>, seguido por
